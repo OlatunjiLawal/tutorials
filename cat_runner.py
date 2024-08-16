d8e75b3fc50759e3549bc97c45f04c98d5e5fb1e
Oluwatoki Lawal <olatunji.lawal@riversafe.co.uk>
2024-08-16 14:16:45 +0100

    Added exception to cat_runner.py

--- cat_runner.py
+++ cat_runner.py
@@ -5,12 +5,15 @@ class GoodCat(Cat):
     def meow(self):
         print("purr purr meow")
     
-
-
 class BadCat(Cat):
 
     def meow(self):
         print("hiss")
+
+class VeryBadCat(Cat):
+
+    def meow(self):
+        raise Exception("Nope. scratch scratch")
 
 print("Good cats go...")
 cat: Cat = GoodCat()
@@ -18,4 +21,8 @@ cat.meow()
 
 print("Bad cats go...")
 cat: Cat = BadCat()
+cat.meow()
+
+print('very bad cats need cages')
+cat: Cat = VeryBadCat()
 cat.meow()
