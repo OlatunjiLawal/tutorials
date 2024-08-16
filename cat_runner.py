4016965345fd2c96a338d0750d930e00fb748da7
Oluwatoki Lawal <olatunji.lawal@riversafe.co.uk>
2024-08-16 14:10:42 +0100

    updating cat_runner.py to include subclasses GoodCat and BadCat.

--- cat_runner.py
+++ cat_runner.py
@@ -1,5 +1,21 @@
 from cat_class import Cat
 
-cat = Cat()
+class GoodCat(Cat):
 
+    def meow(self):
+        print("purr purr meow")
+    
+
+
+class BadCat(Cat):
+
+    def meow(self):
+        print("hiss")
+
+print("Good cats go...")
+cat: Cat = GoodCat()
+cat.meow()
+
+print("Bad cats go...")
+cat: Cat = BadCat()
 cat.meow()
