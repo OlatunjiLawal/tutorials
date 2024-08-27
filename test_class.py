c2512c0b048d73700a626e1683b2fc73d9d891d3
Oluwatoki Lawal <olatunji.lawal@riversafe.co.uk>
2024-08-27 12:02:56 +0100

    updated tests to work with unittest.

--- test_class.py
+++ test_class.py
@@ -1,9 +1,7 @@
 # content of test_class.py
-class TestClass:
+import unittest
+
+class TestClass(unittest.TestCase):
     def test_one(self):
         x = "this"
         assert "h" in x
-
-    def test_two(self):
-        x = "hello"
-        assert hasattr(x, "check")
--- test_fib.py
+++ test_fib.py
@@ -5,26 +5,25 @@ from fib import fibonacci, fib_sequence
 def test_function():
     assert fib_sequence(5) == 3
 
-def test_fib_1(self):
-    self.assertEqual(fibonacci(1), 0, "Expected fibonacci(1) to be 0")
+class TestFib0(unittest.TestCase):
 
-def test_fib_2(self):
-    self.assertEqual(fibonacci(2), 1, "Expected fibonacci(2) to be 1")
+    def test_fib_1(self):
+        self.assertEqual(fibonacci(1), 0, "Expected fibonacci(1) to be 0")
 
-def test_fib_5(self):
-    self.assertEqual(fibonacci(5), 3, "Expected fibonacci(5) to be 3")
+    def test_fib_2(self):
+        self.assertEqual(fibonacci(2), 1, "Expected fibonacci(2) to be 1")
 
-def test_fib_invalid_string(self):
-    with self.assertRaises(TypeError):
-        fibonacci("some-string")
+    def test_fib_5(self):
+        self.assertEqual(fibonacci(5), 3, "Expected fibonacci(5) to be 3")
 
-def test_fib_invalid_float(self):
-    with self.assertRaises(TypeError):
-        fibonacci(5.5)
+    def test_fib_invalid_string(self):
+        with self.assertRaises(TypeError):
+            fibonacci("some-string")
 
-def test_fib_negative(self):
-    with self.assertRaises(ValueError):
-        fibonacci(-5)
+    def test_fib_invalid_float(self):
+        with self.assertRaises(TypeError):
+            fibonacci(5.5)
+
 
 if __name__ == "__main__":
     unittest.main()
